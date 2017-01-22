65afc2355a2471d0affcd2469c2604918c4f0a14
Yi donghoon <icq4ever@gmail.com>
2017-01-22 13:26:33 +0900

    fix pin no with redefined controlModule pin && rename functionName && add some new fn

--- 01_RAMEN/arduinoCode/controlModule/controlModule.ino
+++ 01_RAMEN/arduinoCode/controlModule/controlModule.ino
@@ -1,46 +1,75 @@
-#define PIN_START_BUTTON 	6
-#define PIN_BLOWER_1		A2
-#define PIN_BLOWER_2		A3
-#define PIN_BLOWER_3		A4
-#define PIN_HEATING_MBALL	9
-#define PIN_RELEASE_MBALL	10
-#define PIN_EGG_BREAKER		11
-#define PIN_NOODLE_UP		12
-#define PIN_NOODLE_DOWN		13
+/* 
+ *  RAMEN REMOTE CONTROL MODULE FIRMWARE
+ *  by GradeFreeEngineering Team
+ *  
+ *  Song Hojun 
+ *  Yi Donghoon
+ *  
+ */
 
-#define NUM_OF_OUTPUT		9
+// GFE STANDARD CONTROL PIN LIST
+#define PIN_SOLENOID_1		A0		// PIN_BTN_1	
+#define PIN_SOLENOID_2		A1		// PIN_BTN_2	
+#define PIN_SOLENOID_3		A2		// PIN_BTN_3	
+#define PIN_EGG_BREAKER		A3		// PIN_BTN_4	
+#define PIN_SIREN_1			A4		// PIN_BTN_5	
+#define PIN_SIREN_2			A5		// PIN_BTN_6	
+#define PIN_HOT_WATER		5		// PIN_BTN_7	
+#define PIN_DROP_MBALL		6		// PIN_BTN_8	
+#define PIN_SERVO_UP		9
+#define PIN_SERVO_DOWN		10		
 
-int outputPin[NUM_OF_OUTPUT];		// store pins
-int outputData[NUM_OF_OUTPUT];		// store input of pin
+#define PIN_START_BUTTON	11		// PIN_PWM_1	
+#define PIN_PWM_2			12		// PIN_PWM_2	
+#define PIN_LED 			13		// PIN_LED 	
+
+#define PIN_SPI_SCK	 		SCK		//PIN_SPI_SCK	
+#define PIN_SPI_MOSI 		MOSI	//PIN_SPI_MOSI
+#define PIN_SPI_MISO		MISO	//PIN_SPI_MISO
+
+#define PIN_RX				0		//PIN_RX		
+#define PIN_TX				1		//PIN_TX		
+#define PIN_SDA				2		//PIN_SDA		
+#define PIN_SCL				3		//PIN_SCL		
+
+// number of button message bit
+#define NUM_OF_OUTPUT		10
+
+int inputBufferPin[NUM_OF_OUTPUT];		// store pins
+int messageBuffer[NUM_OF_OUTPUT];		// store input of pin
 
 void setup() {
 	// start Serial vis USB
 	Serial.begin(115200);
 
-	outputPin[0]=PIN_START_BUTTON;
-	outputPin[1]=PIN_BLOWER_1;	
-	outputPin[2]=PIN_BLOWER_2;	
-	outputPin[3]=PIN_BLOWER_3;
-	outputPin[4]=PIN_HEATING_MBALL;
-	outputPin[5]=PIN_RELEASE_MBALL;
-	outputPin[6]=PIN_EGG_BREAKER;
-	outputPin[7]=PIN_NOODLE_UP;
-	outputPin[8]=PIN_NOODLE_DOWN;
+	inputBufferPin[0]	= PIN_SOLENOID_1;	
+	inputBufferPin[1]	= PIN_SOLENOID_2;	
+	inputBufferPin[2]	= PIN_SOLENOID_3;
+	inputBufferPin[3]	= PIN_HEATING_MBALL;
+	inputBufferPin[4]	= PIN_DROP_MBALL;
+	inputBufferPin[5]	= PIN_EGG_BREAKER;
+	inputBufferPin[6]	= PIN_SERVO_UP;
+	inputBufferPin[7]	= PIN_SERVO_DOWN;
+	inputBufferPin[8]	= PIN_SIREN_1;
+	inputBufferPin[9]	= PIN_SIREN_2;
+
+
+	pinMode(PIN_LED, OUTPUT);
 
 	for(int i=0; i<9; i++){
-		outputData[i] = 0;
-		pinMode(outputPin[i], INPUT);
+		messageBuffer[i] = 0;
+		pinMode(inputBufferPin[i], INPUT);
 	}
 }
 
 void loop() {
 
 	for(int i=0; i<NUM_OF_OUTPUT; i++){
-		if(GFEDigitalRead(outputPin[i]))	outputData[i] = 1;
-		else								outputData[i] = 0;
+		if(GFEDigitalRead(inputBufferPin[i]))	messageBuffer[i] = 1;
+		else									messageBuffer[i] = 0;
 	}
 
-	printDataToSerial();
+	sendToActionBoard();
 }
 
 // sudo function definition
@@ -49,11 +78,20 @@ int  GFEAnalogRead(int _pin)					{ return analogRead(_pin);	}
 void GFEDigitalWrite(int _pin, bool _out)		{ digitalWrite(_pin, _out); }
 void GFEAnalogWrite(int _pin, int _out)			{ analogWrite(_pin, _out);	}
 
+void updateStatusLED(){
+	// TODO : update internal LED status
+}
 
-void printDataToSerial(){
+void sendToActionBoard(){
 	for(int i=0; i<NUM_OF_OUTPUT; i++){
 		Serial.print(outputData[i], HEX);
 		Serial.print(",");
 	}
 	Serial.println();
+}
+
+void sendToP5(){
+	// TODO : send to Processing
+	// start?
+
 }
