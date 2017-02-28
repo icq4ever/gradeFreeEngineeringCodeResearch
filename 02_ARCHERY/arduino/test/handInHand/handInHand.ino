69203fd0ba636d0d6b70c9fddd484dfe6946e0d3
U-ICQ4EVER-X250\icq4ever <Yi donghoon>
2017-03-01 01:25:56 +0900

    relay control added : pin 9

--- 02_ARCHERY/arduino/test/handInHand/handInHand.ino
+++ 02_ARCHERY/arduino/test/handInHand/handInHand.ino
@@ -1,13 +1,19 @@
 const byte ledPin = 13;
+const byte relayPin = 9;
+
 const byte interruptPin1 = 2;
 const byte interruptPin2 = 3;
+
 volatile byte state = LOW;
 volatile unsigned int countNum; 
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(relayPin, OUTPUT);
+
   pinMode(interruptPin1, INPUT_PULLUP);
   pinMode(interruptPin2, INPUT);
+
   attachInterrupt(digitalPinToInterrupt(interruptPin1), blink, RISING);
   attachInterrupt(digitalPinToInterrupt(interruptPin2), count, FALLING);
   Serial.begin(115200);
@@ -16,22 +22,31 @@ void setup() {
 }
 
 void loop() {
-  // digitalWrite(ledPin, state);
-  // Serial.println(countNum);
 
-  delay(30);
-  
-  if(countNum > 0)	countNum--;
+	// button Action 
+	// digitalWrite(ledPin, state);
+	
+	// Serial.println(countNum);
 
-  if(countNum > 30)   digitalWrite(ledPin, HIGH);
-  else                digitalWrite(ledPin, LOW);
+	delay(30);
+
+	if(countNum > 0)	countNum--;
+
+
+	if(countNum > 30)   {
+		digitalWrite(ledPin, HIGH);
+		digitalWrite(relayPin, HIGH);
+	} else  {
+		digitalWrite(ledPin, LOW);
+		digitalWrite(relayPin, LOW);
+	}
 }
 
 void blink() {
-  state = !state;
+	state = !state;
 }
 
 void count(){
 	if(countNum < 40)  countNum++;
-  else               countNum = 40;
+ 	else               countNum = 40;
 }
