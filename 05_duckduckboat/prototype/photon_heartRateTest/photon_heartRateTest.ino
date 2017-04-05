f9e17acd37b0e539a63cc94dd127a4cd792836c0
Yi donghoon <icq4ever@gmail.com>
2017-04-05 15:13:14 +0900

    ISR work. OSC, publish works

--- 05_duckduckboat/prototype/photon_heartRateTest/photon_heartRateTest.ino
+++ 05_duckduckboat/prototype/photon_heartRateTest/photon_heartRateTest.ino
@@ -1,117 +1,117 @@
-/* 
-	particle photon example!!! 
-*/
+#define LED     D7                  //indicator, Grove - LED is connected with D4 of Arduino
+#define ISRPIN  D2
 
 #include "simple-OSC.h"
 
 UDP udp;
 
-IPAddress outIp(192, 168, 100, 102);//your computer IP
-unsigned int outPort = 8000; //computer incoming port
+IPAddress outIP(192, 168, 100, 103);
+unsigned int outPort = 8000;
 
-unsigned char counter;
+
+bool bPulseWorking = false;
+volatile bool ledState = LOW;       
+
+unsigned int counter;
 unsigned long temp[7];
 unsigned long sub;
 bool dataOK = true;
-unsigned int heartRate;
-volatile bool ledState = LOW;
 
-const int maxHeartRatePulseDuty = 2000;
+unsigned int heartRate=0;
+unsigned long lastPublishedTimer = 0;
+const long maxHeartRatePulseDuty = 2000;
 
 void setup() {
-    
     // static IP setting
-    IPAddress myAddress(192, 168, 100, 101);
+    IPAddress myAddress(192, 168, 100, 101);            // 102 : donghoon MBP
     IPAddress netmask(255, 255, 255, 0);
     IPAddress gateway(192, 168, 100, 1);
-    IPAddress dns(192, 168, 100, 1);
+    IPAddress dns(8,8,8,8);
     WiFi.setStaticIP(myAddress, netmask, gateway, dns);
     
     WiFi.useStaticIP();
- 
-    Serial.begin(115200);
-    udp.begin(0);//necessary even for sending only.
-    while (!WiFi.ready()) {
+    
+    udp.begin(115200);
+    while(!WiFi.ready()){
         delay(500);
-        Serial.print(".");
     }
-    Serial.println("");
-    Serial.println("WiFi connected");
     
     IPAddress ip = WiFi.localIP();
-  
+    
     char message[64] = "";
     sprintf(message, "My Local IP= %d:%d:%d:%d", ip[0], ip[1], ip[2], ip[3]);
     Particle.publish("myLocalIP", message);
     
-    // setup heartRate
+    pinMode(LED, OUTPUT);
     delay(5000);
-    pinMode(D7, OUTPUT);
-    pinMode(D3, INPUT);
-    Particle.publish("LOG", "heartRate test begin");
-    attachInterrupt(D3, heart, RISING);
+    arrayInit();
+    Particle.publish("LOG", "heartRate test begin...");
+    attachInterrupt(ISRPIN, interrupt, RISING);
+    
+    lastPublishedTimer = millis();
 }
 
 void loop() {
-    digitalWrite(D7, ledState);
+    digitalWrite(LED, ledState);//Update the state of the indicator
+    
+    if(millis() - lastPublishedTimer > 2000){
+        Particle.publish("HeartRate", intToString(heartRate));
+        
+        // publish heartRate via OSC 
+        OSCMessage outMessage("/heartRate/demo");
+        outMessage.addInt(heartRate);
+        outMessage.send(udp,outIP,outPort);
+        lastPublishedTimer = millis();
+    }
+    
+    
 }
 
 void getHeartRate(){
     if(dataOK){
-        heartRate = 360000/(temp[6] - temp[0]);
-        Particle.publish("HeartRate", heartRate);
-        
-        //SEND
-        OSCMessage outMessage("/pulseRate");
-        outMessage.addFloat(heartRate);
-        outMessage.send(udp,outIp,outPort);
-        // delay(3000);
-    }
+        heartRate = 360000 / (temp[6] - temp[0]);
+    } 
 }
 
-void heart(){
+void interrupt(){
+    ledState = !ledState;
+    
     noInterrupts();
-    Particle.publish("LOG", "HEART!");
+    
     temp[counter] = millis();
     
-    switch(counter){
-        case 0:
-            sub = temp[counter] - temp[6];
-            // Particle.publish("sub", sub);
-            break;
-        default:
-            sub = temp[counter] - temp[counter-1];
-            // Particle.publish("sub", sub);
-            break;
-    }
+    if(counter == 0)    sub = temp[counter] - temp[6];
+    else                sub = temp[counter] - temp[counter-1];
     
-    if(sub>maxHeartRatePulseDuty) {
+    if(sub > maxHeartRatePulseDuty){
         dataOK = false;
         counter = 0;
-        // Particle.publish("LOG", "HeartRate measure failed... restart!");
         arrayInit();
-    }
+    } 
     
     if(counter == 6 && dataOK){
-        counter =0;
-        // getHeartRate();
-    }
-    
-    else if(counter != 6 && dataOK){
+        counter = 0; 
+        getHeartRate();
+    }  else if (counter!= 6 && dataOK){
         counter++;
-        ledState = !ledState;
-    }
-    
-    else {
-        counter = 0;
-        dataOK = false;
+        // ledState != ledState;
+    } else {
+        counter=0;
+        dataOK = true;
     }
     interrupts();
 }
 
 void arrayInit(){
-    for(unsigned int i=0; i<6; i++){
+    heartRate = 0;
+    for(int i=0; i<6; i++){
         temp[i] = 0;
         temp[6] = millis();
     }
+}
+
+String intToString(int num){
+    char message[64] = "";
+    sprintf(message, "%d", num);
+    return message;
 }
